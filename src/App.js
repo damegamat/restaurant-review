5f42ef1145c5428a9a3b228b7c452691a3ece9ba
damegamat <damegamat4@gmail.com>
2020-05-15 11:35:45 +0200

    add route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import GlobalStyles from "./index.css";
 
 import theme from "utils/theme";
 import { Navigation } from "components";
-import { Home } from "./page";
+import { Home, Details, Register } from "./page";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <div className="App">
-        <Navigation />
-        <Home />
-      </div>
+      <Router>
+        <Navigation item={[{ component: "Home", path: "/" }]} />
+        <Switch>
+          <Route>
+            <Home exact path="/" />
+          </Route>
+          <Route>
+            <Details path="/details/:id" />
+          </Route>
+          <Route>
+            <Register path="/register" />
+          </Route>
+        </Switch>
+      </Router>
     </ThemeProvider>
   );
 }
