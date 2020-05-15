1f0ced760c448825222a4d2faa017b1b78318d17
damegamat <damegamat4@gmail.com>
2020-05-15 12:57:28 +0200

    add import

--- src/page/index.js
+++ src/page/index.js
@@ -1,3 +1,4 @@
 export { default as Home } from "./Home";
 export { default as Details } from "./Details";
 export { default as Register } from "./Register";
+export { default as SignIn } from "./SignIn";
