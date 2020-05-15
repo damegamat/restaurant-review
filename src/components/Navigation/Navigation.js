00948cd2b9027d4a9515888b7557c4e4ee5ffa65
damegamat <damegamat4@gmail.com>
2020-05-15 16:02:45 +0200

    update navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,27 @@
-import React from "react";
-import { Link, useHistory } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useHistory, useLocation } from "react-router-dom";
 import { AppBar, Button, IconButton } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 import { Navigation as S } from "./Navigation.css";
 
 export default function Navigation({ items }) {
+  const [pass, setPass] = useState(true);
+  const [btnText, setBtnText] = useState("");
+  console.log(btnText);
   let history = useHistory();
+  const local = useLocation();
+
+  useEffect(() => {
+    if (pass) {
+      setBtnText("Logout");
+    } else {
+      if (local.pathname == "/register") {
+        setBtnText("Signin");
+      } else if (local.pathname == "/signin") {
+        setBtnText("Register");
+      }
+    }
+  }, [pass]);
 
   const item = items.map((el) => {
     return (
@@ -15,8 +31,20 @@ export default function Navigation({ items }) {
     );
   });
 
-  const handleClick = () => {
-    history.push("/signin");
+  const handleClick = (btnText) => {
+    if (pass) {
+      setBtnText("Register");
+      history.push("/signin");
+      setPass(false);
+    } else {
+      if (btnText === "Register") {
+        setBtnText("Signin");
+        history.push("/register");
+      } else {
+        setBtnText("Register");
+        history.push("/signin");
+      }
+    }
   };
   return (
     <AppBar position="static">
@@ -29,8 +57,8 @@ export default function Navigation({ items }) {
           </S.Item>
           {item}
         </S.Nav>
-        <Button onClick={handleClick} color="inherit">
-          Logout
+        <Button onClick={() => handleClick(btnText)} color="inherit">
+          {btnText}
         </Button>
       </S.Toolbar>
     </AppBar>
